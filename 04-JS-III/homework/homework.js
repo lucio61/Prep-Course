125037edeeaaf235b5f8ecd8f8f34d9e80381c45
lucio61 <101020474+lucio61@users.noreply.github.com>
2022-03-25 17:32:23 -0300

    04 en proceso

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -119,13 +119,25 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
-}
+  if(arguments.length === 0) return 0
 
-
-function cuentoElementos(arreglo){
+  var acc = 1
+  for (let i = 0; i < arguments.length; i++) {
+    acc = acc * arguments[i]
+  }
+  return acc 
+ } 
+  
+ function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  var contador = 0
+  for (let i = 0; i < arreglo.length; i++){
+    if (arreglo [i] > 18 ){
+      contador = contador + 1
+    }
+  }
+  return contador
 }
 
 
@@ -133,8 +145,13 @@ function diaDeLaSemana(numeroDeDia) {
   //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
-  //Escribe tu código aquí   
-  
+  //Escribe tu código aquí 
+
+  if ( numeroDeDia === 1 || numeroDeDia === 7 ) {
+    return "Es fin de semana"
+  } else {
+    return "Es dia Laboral" 
+  }
 } 
 
 
@@ -142,7 +159,12 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  var nStr = n.toString()
+  if (nStr[0] === "9") {
+    return true
+  } else{
+    return false
+  }
 }
 
 
@@ -150,7 +172,13 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  for ( let i = 0; i < arreglo.length-1; i++){
+    if (arreglo[i] === arreglo[i+1]){
+      return false
+    }
+  }
+  return true
+
 } 
 
 
